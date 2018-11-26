619bc3f8388296dcf80f1a78efdd8cec8ae8f294
Nick Teff <nicholas.teff@gmail.com>
2018-11-25 21:58:38 -0600

    Minor tweaks to frontend view.

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-import {Row, Col, Select, Card} from 'antd';
+import {Row, Col, Select, Layout} from 'antd';
 import "antd/dist/antd.css";
 //import Select from 'react-select';
 import Plot from 'react-plotly.js';
 import axios from 'axios';
+
+
+const { Header, Content, Footer } = Layout;
 
 const Option = Select.Option
 
@@ -23,10 +26,10 @@ const layout = {
     style:'streets'
   },
   margin: {
-    l: 0,
-    r: 5,
-    b: 10,
-    t: 5,
+    l: 1,
+    r: 1,
+    b: 1,
+    t: 1,
     pad: 4
   },
 }
@@ -68,6 +71,7 @@ class Map extends Component {
     })
     console.log(opt)
     console.log(this.selectValue)
+
     // this is still not quite working.  I want the layout to update
     // but having some issues getting it from the API
     axios.get(states+'/'+opt)
@@ -115,59 +119,67 @@ class Map extends Component {
         className="App"
         style={{
           paddingTop: '10px',
-          paddingLeft: '5%',
-          paddingRight: '-5%',
+          //paddingLeft: '5%',
+          //paddingRight: '-5%',
           background:'#ECECEC'
         }}>
-
-          <Card
-            title="US Census"
-            style={{
-              width: '95%',
-              height: '95%',
-            }}
-            hoverable
-          >
-            <Row>
-              <Col span={6}>
-                  <Select
-                    showSearch
-                    defaultValue="Urban or rural"
-                    style={{ width: '100%' }}
-                    onChange={ this.onUrbanSelectChange }
-                  >
-                      <Option key='urban'>Urban </Option>
-                      <Option key='rural'>Rural </Option>
-                  </Select>
-              </Col>
-              <Col span={6}>
-              <Select
-                showSearch
-                defaultValue="Select a state"
-                style={{ width: '100%' }}
-                onChange={ this.onSelectChange }
-              >
-                { this.state.options.map(st =>
-                  <Option key={ st.value }>{ st.label } </Option>) }
-              </Select>
-            </Col>
-            </Row>
-            <Plot
-              data={ this.state.data }
-              layout={ this.state.layout }
-              style={
-                {
-                  width: '75%',
-                  height: '50%',
-                }
-               }
-              useResizeHandler={ true }
-              config={{
-                mapboxAccessToken: AccessToken,
-                displaylogo: false,
-              }}>
-            </Plot>
-          </Card>
+          <Layout>
+            <Header theme='light'>
+              <h2 className='logo'>US Census</h2>
+            </Header>
+            <Content>
+              <div
+                style={{
+                  marginTop:'1%',
+                  marginLeft:'2%',
+                  marginRight:'2%',
+                }}
+                >
+                <Row>
+                  <Col span={6}>
+                      <Select
+                        showSearch
+                        defaultValue="Urban or rural"
+                        style={{ width: '100%' }}
+                        onChange={ this.onUrbanSelectChange }
+                      >
+                          <Option key='urban'>Urban </Option>
+                          <Option key='rural'>Rural </Option>
+                      </Select>
+                  </Col>
+                  <Col span={18}>
+                    <Select
+                      showSearch
+                      defaultValue="Select a state"
+                      style={{ width: '100%' }}
+                      onChange={ this.onSelectChange }
+                    >
+                      { this.state.options.map(st =>
+                        <Option key={ st.value }>{ st.label } </Option>) }
+                    </Select>
+                  </Col>
+                </Row>
+                <Row>
+                  <Plot
+                    data={ this.state.data }
+                    layout={ this.state.layout }
+                    style={
+                      {
+                        width: '100%',
+                        height: '100%',
+                      }
+                     }
+                    useResizeHandler={ true }
+                    config={{
+                      mapboxAccessToken: AccessToken,
+                      displaylogo: false,
+                    }}>
+                  </Plot>
+                </Row>
+              </div>
+            </Content>
+            <Footer></Footer>
+          </Layout>
       </div>
     );
   }
